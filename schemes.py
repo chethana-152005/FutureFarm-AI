caec89eb419814130cb6704c64eeb0bf9db5aea9
vukkalkar chethana <vukkalkarc@gmail.com>
2026-03-10 12:54:03 +0530

    Add function to retrieve agricultural schemes

---
+++ schemes.py
@@ -0,0 +1,25 @@
+def get_schemes():
+
+    schemes = {
+
+    "PM-KISAN":
+    "Provides ₹6000 per year income support to farmers",
+
+    "PMFBY":
+    "Pradhan Mantri Fasal Bima Yojana provides crop insurance",
+
+    "Soil Health Card":
+    "Provides soil nutrient status to farmers",
+
+    "PMKSY":
+    "Pradhan Mantri Krishi Sinchai Yojana improves irrigation",
+
+    "eNAM":
+    "National Agriculture Market for better crop selling",
+
+    "KCC":
+    "Kisan Credit Card provides easy farm loans"
+
+    }
+
+    return schemes
